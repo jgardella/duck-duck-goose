ae998c2a345423e4d2bdfa667b7c2e72fe875045
jgardella <jgardell@stevens.edu>
2015-06-02 22:48:44 -0400

    Implement Event class.

---
+++ src/main/java/com/jgardella/app/backend/Event.java
@@ -0,0 +1,51 @@
+import java.time.LocalDateTime;
+
+class Event implements Comparable<Event>
+{
+	private String name;
+	private String type;
+	private LocalDateTime date;
+
+	public Event(String name, String type, LocalDateTime date)
+	{
+		this.name = name;
+		this.type = type;
+		this.date = date;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	public String getType()
+	{
+		return type;
+	}
+
+	public void setType(String type)
+	{
+		this.type = type;
+	}
+
+	public LocalDateTime getDate()
+	{
+		return date;
+	}
+
+	public void setDate(LocalDateTime date)
+	{
+		this.date = date;
+	}
+
+	public int compareTo(Event event)
+	{
+		return date.compareTo(event.date);
+	}
+
+}
