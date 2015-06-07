f6040e50a714d8f6e568933b2605e08dd7de6759
Jason Gardella <jgardell@stevens.edu>
2015-06-06 21:19:45 -0400

    Implement requirement class.

---
+++ src/main/java/com/jgardella/app/backend/Requirement.java
@@ -0,0 +1,34 @@
+package com.jgardella.app;
+
+// Attendance requirement definition
+public class Requirement
+{
+	private ReqType reqType;
+	private int amount;
+	private String[] eventTypes;
+
+	public enum ReqType { CONSECUTIVE, ABSOLUTE, LAST };
+
+	public Requirement(ReqType type, int amount, String... eventTypes)
+	{
+		this.reqType = type;
+		this.amount = amount;
+		this.eventTypes = eventTypes;
+	}
+
+	public ReqType getType()
+	{
+		return reqType;
+	}
+
+	public int getAmount()
+	{
+		return amount;
+	}
+
+	public String[] getEventTypes()
+	{
+		return eventTypes;
+	}
+
+}
