5158a6e9ea1668a5cf286a9c6f7bdcd519a2cc46
Jason Gardella <jgardell@stevens.edu>
2015-06-07 17:00:54 -0400

    Finish implementing MembershipEvaluator.

--- src/main/java/com/jgardella/app/backend/MembershipEvaluator.java
+++ src/main/java/com/jgardella/app/backend/MembershipEvaluator.java
@@ -6,6 +6,10 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Collections;
 
+import com.jgardella.app.backend.Requirement;
+import com.jgardella.app.backend.Requirement.ReqType;
+import com.jgardella.app.backend.Member.Status;
+
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 
 public class MembershipEvaluator
@@ -15,12 +19,17 @@ public class MembershipEvaluator
 	private HashMap<String, ArrayList<Event>> eventMap;
 	private ArrayList<Requirement> reqList;
 
+	private ArrayList<Member> activeMembers;
+	private ArrayList<Member> inactiveMembers;
+
 	/**
 	   Parses the given event type directories, generating an internal member list and event map.
 	   @param File... eventDirs The event type directories containing the attendance sheets.
 	  **/
 	public void parseEvents(File... eventDirs) throws IOException, InvalidFormatException
 	{
+		memberList = new ArrayList<Member>();
+		eventMap = new HashMap<String, ArrayList<Event>>();
 		for(File eventDir : eventDirs)
 		{
 			parseEvent(eventDir);
@@ -61,22 +70,107 @@ public class MembershipEvaluator
 
 	public void evaluateMembership()
 	{
+		activeMembers = new ArrayList<Member>(memberList); // active member list contains all members at start
+		inactiveMembers = new ArrayList<Member>(); // inactive members list empty
 
+		for(Member member : memberList)
+		{
+			member.setStatus(Status.ACTIVE);
+			for(Requirement req : reqList)
+			{
+				boolean reqMet = processRequirement(member, req);
+				if(!reqMet) // if requirement not met, set status inactive and break
+				{
+					member.setStatus(Status.INACTIVE); // set member inactive
+					activeMembers.remove(member); // remove member from active list
+					inactiveMembers.add(member); // add member to inactive list
+					break;
+				}
+			}
+		}
 	}
 
+	private boolean processRequirement(Member member, Requirement req)
+	{
+		ArrayList<Event> eventList = eventMap.get(req.getEventType());
+		switch(req.getType())
+		{
+		case ABSOLUTE:
+			int numAbs = 0;
+			for(Event event : eventList)
+			{
+				if(event.getAttendance().contains(member))
+				{
+					numAbs++;
+					if(numAbs >= req.getAmount())
+					{
+						return true;
+					}
+				}
+			}
+			return false;
+		case CONSECUTIVE:
+			int numConsecutive = 0;
+			for(Event event : eventList)
+			{
+				if(event.getAttendance().contains(member))
+				{
+					numConsecutive++;
+					if(numConsecutive >= req.getAmount())
+					{
+						return true;
+					}
+				}
+				else
+				{
+					numConsecutive = 0;
+				}
+			}
+			return false;
+		case LAST:
+			int numLast = 0;
+			for(Event event : eventList)
+			{
+				if(event.getAttendance().contains(member))
+				{
+					numLast++;
+					if(numLast >= req.getAmount())
+					{
+						return true;
+					}
+				}
+				else
+				{
+					return false;
+				}
+			}
+			return false;
+		}
+		return false;
+	}
+
+	/**
+	  Returns a list of active members. This method will return null if evaluateMembership() has not been called yet.
+	  **/
 	public ArrayList<Member> getActiveMembers()
 	{
-
+		return activeMembers;
 	}
 
+	/**
+	  Returns a list of inactive members. This method will return null if evaluateMembership() has not been called yet.
+	  **/
 	public ArrayList<Member> getNonactiveMembers()
 	{
-
+		return inactiveMembers;
 	}
 
+	/**
+	  Returns a list of all members. This method will return null if parseEvents() has not been called yet.
+	  **/
 	public ArrayList<Member> getAllMembers()
 	{
-
+		return memberList;
 	}
 
 }
