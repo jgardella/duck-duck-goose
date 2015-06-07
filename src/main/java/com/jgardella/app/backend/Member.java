d9d05dc93b1f5d9ef16b5613ab51bfe81958f01d
Jason Gardella <jgardell@stevens.edu>
2015-06-07 17:00:23 -0400

    Implement equals and hashCode in Member.

--- src/main/java/com/jgardella/app/backend/Member.java
+++ src/main/java/com/jgardella/app/backend/Member.java
@@ -5,12 +5,16 @@ public class Member
 	private String firstName;
 	private String lastName;
 	private int id;
+	private Status status;
+
+	public enum Status { ACTIVE, INACTIVE, UNDEF };
 
 	public Member(String firstName, String lastName, int id)
 	{
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.id = id;
+		this.status = Status.UNDEF;
 	}
 
 	public String getFullName()
@@ -47,4 +51,32 @@ public class Member
 	{
 		this.id = id;
 	}
+
+	public void setStatus(Status status)
+	{
+		this.status = status;
+	}
+
+	public Status getStatus()
+	{
+		return status;
+	}
+
+	@Override
+	public boolean equals(Object obj)
+	{
+		if(obj instanceof Member)
+		{
+			Member member = (Member) obj;
+			return this.id == member.id;
+		}
+		return false;
+	}
+
+	@Override
+	public int hashCode()
+	{
+		return id;
+	}
+
 }
