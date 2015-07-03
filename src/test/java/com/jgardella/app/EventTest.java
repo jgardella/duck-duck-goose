cb58dfeed3f703250e1b186aa19647fc4e9ce1e1
Jason Gardella <jgardell@stevens.edu>
2015-07-02 21:35:25 -0400

    Fix event ordering test.

--- src/test/java/com/jgardella/app/EventTest.java
+++ src/test/java/com/jgardella/app/EventTest.java
@@ -15,7 +15,7 @@ public class EventTest
 	{
 		Event earlierEvent = new Event("Earlier Event", "meeting", LocalDateTime.of(2015, Month.FEBRUARY, 1, 0, 0));
 		Event moreRecentEvent = new Event("More Recent Event", "meeting", LocalDateTime.of(2015, Month.JANUARY, 1, 0, 0));
-		assertTrue("Earlier event should come after more recent event.", earlierEvent.compareTo(moreRecentEvent) > 0);
+		assertTrue("Earlier event should come after more recent event.", earlierEvent.compareTo(moreRecentEvent) < 0);
 	}
 
 }
